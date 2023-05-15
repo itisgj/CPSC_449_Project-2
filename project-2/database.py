f5980fd60544c6904e5836f3c58159cd6e99b993
itisgj <53045468+itisgj@users.noreply.github.com>
2023-05-14 19:08:00 -0700

    Update database.py

--- project-2/database.py
+++ project-2/database.py
@@ -9,7 +9,3 @@ client = pymongo.MongoClient(MONGO_URI)
 
 database = client["bookstore"]
 collection = database["books"]
-
-# collection.create_index([("title", pymongo.TEXT)])
-# collection.create_index([("author", pymongo.TEXT)])
-# collection.create_index([("price", pymongo.ASCENDING)])
